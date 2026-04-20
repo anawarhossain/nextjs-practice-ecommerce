28e9a442fb801cb36902156febdb97f5fd1314b9
Anawar Hossain <anwarhossainhossain01@gmail.com>
2026-04-21 01:35:20 +0600

    Routing and 404 not found practice

---
+++ src/app/not-found.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFountPage = () => {
+    return (
+        <div className='flex flex-col h-screen justify-center items-center'>
+            <h1>404</h1>
+            <h1>Not Found Page</h1>
+        </div>
+    );
+};
+
+export default NotFountPage;
